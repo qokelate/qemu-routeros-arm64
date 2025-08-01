766345a40cd27bc0e503f8e94e156f3887d4abb2
Your Name <you@example.com>
2025-08-01 12:27:08 +0800

    v1

--- ros.sh
+++ ros.sh
@@ -37,19 +37,22 @@ nmcli connection up br-wan
 nmcli connection modify --temporary br-kvm ipv4.method auto ipv6.method disabled -ipv4.dns '' -ipv4.gateway '' -ipv4.addresses '' -ipv4.routes ''
 nmcli connection up br-kvm
 
+
 sleep 5
+
+
+netopt="queues=4,vhost=on"
+drvopt="mq=on,vectors=8"
 
 qemu-system-aarch64 -nographic \
 -m 512M -M virt -enable-kvm -cpu host \
 -bios "/usr/share/qemu-efi-aarch64/QEMU_EFI.fd" \
--netdev 'bridge,br=br-lan1,id=n1' -device 'virtio-net-pci,netdev=n1,mac=d2:67:e2:11:11:11' \
--netdev 'bridge,br=br-lan2,id=n2' -device 'virtio-net-pci,netdev=n2,mac=d2:67:e2:22:22:22' \
--netdev 'bridge,br=br-wan,id=n3' -device 'virtio-net-pci,netdev=n3,mac=d2:67:e2:33:33:33' \
--netdev 'bridge,br=br-kvm,id=n4' -device 'virtio-net-pci,netdev=n4,mac=d2:67:e2:44:44:44' \
--device 'nvme,drive=hd0,serial=BDCF8C72-9BE7-4118-B274-EAD8B0982915,bootindex=0' \
+-netdev "tap,br=br-lan1,id=n1,$netopt" -device "virtio-net-pci,netdev=n1,mac=d2:67:e2:11:11:11,$drvopt" \
+-netdev "tap,br=br-lan2,id=n2,$netopt" -device "virtio-net-pci,netdev=n2,mac=d2:67:e2:22:22:22,$drvopt" \
+-netdev "tap,br=br-wan,id=n3,$netopt" -device "virtio-net-pci,netdev=n3,mac=d2:67:e2:33:33:33,$drvopt" \
+-netdev "tap,br=br-kvm,id=n4,$netopt" -device "virtio-net-pci,netdev=n4,mac=d2:67:e2:44:44:44,$drvopt" \
+-device "nvme,drive=hd0,serial=BDCF8C72-9BE7-4118-B274-EAD8B0982915,bootindex=0" \
 -drive "if=none,id=hd0,media=disk,discard=unmap,detect-zeroes=unmap,format=qcow2,file=$PWD/ros-7.16.2-arm64.qcow2"
-
-
 
 
 nmcli connection modify --temporary br-lan1 ipv4.method auto ipv6.method disabled -ipv4.dns '' -ipv4.gateway '' -ipv4.addresses '' -ipv4.routes ''
